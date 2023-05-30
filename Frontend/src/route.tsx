cd3f9dae5cc0b20ee0a3c51029a516c9626ad14b
Phoo Phoo <zinphoomon.zpm@gmail.com>
2023-05-30 09:19:55 +0630

    route

--- Frontend/src/route.tsx
+++ Frontend/src/route.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Dashboard from "./pages/dashboard/dashboard";
 import Project from "./pages/projects/projects";
@@ -15,10 +16,6 @@ import UserList from "./pages/user_list/user_list_connect";
 import ClientEdit from "./pages/client_edit/client_edit_contex";
 import ClientDelete from "./pages/client_edit/client_delete";
 import QuotationForm from "./pages/quotation/quotation";
-
-import { Logout } from "./pages/auth/logout";
-import { useEffect, useState } from "react";
-
 import CategoryList from "./pages/category_list/categoryList";
 import CategoryCreate from "./pages/category_create/categoryCreate";
 import CategoryDelete from "./pages/category_edit/category_delete";
@@ -27,148 +24,7 @@ import PermissionList from "./pages/permission_list/permissionList";
 import RoleList from "./pages/role_list/roleList";
 import RoleEdit from "./pages/role_edit/roleEdit";
 
-
-/*
-[
-  {
-  "route":"dashboard"
-},
-{
-  "route":"clients-list"
-}
-]
-*/
-
-// const backend_routes = ['dashboard', 'users', 'services']
-
-// const routes = [
-//   {
-//     path: '/',
-//     backend_path: "dashboard",
-//     element: (
-//       <Dashboard />
-//     )
-//   },
-//   {
-//     path: '/client-lists',
-//     backend_path: "clients-lists",
-//     element: (
-//       <ClientList />
-//     )
-//   },
-//   {
-//     path: '/client-project-lists',
-//     backend_path: 'client-project-lists',
-//     element: (
-//       <ClientProjectList />
-//     )
-//   },
-//   {
-//     path: '/client-projects',
-//     backend_path: 'client-projects',
-//     element: (
-//       <Project />
-//     )
-//   },
-//   {
-//     path: '/project-detail',
-//     backend_path: 'project-detail',
-//     element: (
-//       <ProjectDetail />
-//     )
-//   },
-//   {
-//     path: '/add-client-project',
-//     backend_path: 'add-client-project',
-//     element: (
-//       <ProjectCreate />
-//     )
-//   },
-//   {
-//     path: '/projects',
-//     backend_path: 'projects',
-//     element: (
-//       <Project />
-//     )
-//   },
-//   {
-//     path: '/users',
-//     backend_path: 'users',
-//     element: (
-//       <UserList />
-//     )
-//   },
-//   {
-//     path: '/services',
-//     backend_path: 'services',
-//     element: (
-//       // <Service/>
-//       <h1>Service</h1>
-//     )
-//   },
-//   {
-//     path: '/client-create',
-//     backend_path: 'client-create',
-//     element: (
-//       <ClientCreate />
-//     )
-//   },
-//   {
-//     path: '/client_edit/:customerId',
-//     backend_path: 'client_edit',
-//     element: <ClientEdit />
-//   },
-//   {
-//     path: "/client_delete/:customerId",
-//     backend_path: "client_delete",
-//     element: <ClientDelete />
-//   },
-//   {
-//     path: '/login',
-//     backend_path: 'login',
-//     element: (
-//       <Login email="" password="" />
-//     )
-//   },
-//   {
-//     path: '/logout',
-//     backend_path: 'logout',
-//     element: (
-//       <Logout />
-//     )
-//   },
-//   {
-//     path: '/user_create',
-//     backend_path: 'user_create',
-//     element: (
-//       <RegisterForm />
-//     )
-//   },
-//   {
-//     path: '/user_edit/:userId',
-//     backend_path: 'user_edit',
-//     element: (
-//       <UserEditFrom />
-//     )
-//   },
-//   {
-//     path: '/user_delete/:userId',
-//     backend_path: 'user_delete',
-//     element: (
-//       <UserDelete />
-//     )
-//   },
-//   {
-//     path: '/quotation',
-//     backend_path: 'quotation',
-//     element: (
-//       <QuotationForm />
-//     )
-//   },
-
-// ];
-
-const routes = [
+const routeList = createBrowserRouter([
   {
     path: '/',
     element: (
@@ -178,57 +34,55 @@ const routes = [
   {
     path: '/client-lists',
     element: (
-      <ClientList />
+       <ClientList />
     )
   },
   {
     path: '/client-project-lists',
     element: (
-      <ClientProjectList />
+       <ClientProjectList />
     )
   },
   {
     path: '/client-projects',
     element: (
-      <Project />
+        <Project />
     )
   },
   {
     path: '/project-detail',
     element: (
-      <ProjectDetail />
+        <ProjectDetail />
     )
   },
   {
     path: '/add-client-project',
     element: (
-      <ProjectCreate />
+        <ProjectCreate/>
     )
   },
   {
     path: '/projects',
     element: (
-      <Project />
+        <Project />
     )
   },
   {
-    path: '/users',
-    element: (
-      <UserList />
+    path:'/users',
+    element:(
+      <UserList/>
     )
   },
   {
     path: '/services',
     element: (
-
-      // <Service/>
-      <h1>Service</h1>
-    )
+        <h1>Service</h1>
+   )
   },
   {
     path: '/client-create',
     element: (
-      <ClientCreate />
+        <ClientCreate />
     )
   },
   {
@@ -236,19 +90,13 @@ const routes = [
     element: <ClientEdit />
   },
   {
-    path: "/client_delete/:customerId",
-    element: <ClientDelete />
+    path:"/client_delete/:customerId",
+     element:<ClientDelete />
   },
   {
     path: '/login',
     element: (
       <Login email="" password="" />
-    )
-  },
-  {
-    path: '/logout',
-    element: (
-      <Logout />
     )
   },
   {
@@ -258,62 +106,62 @@ const routes = [
     )
   },
   {
-    path: '/user_edit/:userId',
-    element: (
-      <UserEditFrom />
+    path:'/user_edit/:userId',
+    element:(
+      <UserEditFrom/>
     )
   },
   {
-    path: '/user_delete/:userId',
-    element: (
-      <UserDelete />
+    path:'/user_delete/:userId',
+    element:(
+      <UserDelete/>
     )
   },
   {
-    path: '/quotation',
-    element: (
-      <QuotationForm />
+    path:'/quotation',
+    element:(
+      <QuotationForm/>
     )
   },
   {
-    path: '/category-list',
-    element: (
-      <CategoryList />
+    path:'/category-list',
+    element:(
+      <CategoryList/>
     )
   },
   {
-    path: '/category-create',
-    element: (
-      <CategoryCreate />
+    path:'/category-create',
+    element:(
+      <CategoryCreate/>
     )
   },
   {
-    path: '/category-edit/:categoryId',
-    element: (
+    path:'/category-edit/:categoryId',
+    element:(
       <CategoryEdit />
     )
   },
   {
-    path: '/category-delete/:categoryId',
-    element: (
+    path:'/category-delete/:categoryId',
+    element:(
       <CategoryDelete />
     )
   },
   {
-    path: '/permission-list',
-    element: (
+    path:'/permission-list',
+    element:(
       <PermissionList />
     )
   },
   {
-    path: '/role-list',
-    element: (
+    path:'/role-list',
+    element:(
       <RoleList />
     )
   },
   {
-    path: '/role-edit/:roleId',
-    element: (
+    path:'/role-edit/:roleId',
+    element:(
       <RoleEdit />
     )
   },
@@ -323,48 +171,10 @@ const routes = [
       <Error />
     )
   }
-
-];
-
-const notFoundRoute = {
-  path: '*',
-  element: (
-    <Error />
-  )
-}
+]);
 
 export const Router = () => {
-
-  /*
-   1. fetch avialable route for logged in user from the backend.
-  
-  */
-  // const [roles, setRoles] = useState<any[]>([notFoundRoute]);
-  // const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  // useEffect(() => {
-  //   async function fetchRoles() {
-  //     try {
-  //       // const response = await fetch("/roles");
-  //       // const roles = await response.json()
-  //       const avialable_routes = routes.filter(route => backend_routes.includes(route.backend_path)).map(route => ({ element: route.element, path: route.path }))
-  //       console.log(avialable_routes)
-  //       setRoles(prev => [...avialable_routes, ...prev,]);
-  //     } catch (error) {
-  //       console.log(error)
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   }
-  //   fetchRoles();
-  // }, [])
-  // if (isLoading) {
-  //   return <p>Loading..........</p>;
-  // }
-  // console.log({ roles })
-  console.log(routes)
-  const routeList = createBrowserRouter(routes);
-  return (
-    <RouterProvider router={routeList} />
+  return(
+    <RouterProvider router={routeList}/>
   )
 }
